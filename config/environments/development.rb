564fffa8a7fb4104256784d715abdb454e4a7b50
Timothy Angus <timangus321@gmail.com>
2019-05-20 12:24:20 +0200

    Updated gem to include devise so all login stuff is done

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
@@ -33,7 +35,7 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.perform_caching = false
+  config.action_mailer.perform_caching =
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
