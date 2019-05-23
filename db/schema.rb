c2258528db54e0c506d8fea87c41f6e9a62fdd9d
Fabio Bernhard <bernhardfabio@gmail.com>
2019-05-23 18:21:07 +0200

    minor changes to _index.scss and bug fixes

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_23_092809) do
-  
+ActiveRecord::Schema.define(version: 2019_05_23_132116) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
