b48e3210f5e27489755d77db02de4df42438283d
Timothy Angus <timangus321@gmail.com>
2019-05-23 14:49:06 +0200

    Small fixes to datepicker

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :caves, only: [:index, :show, :new, :create, :destroy] do
-    resources :reservations, only: [:new, :create, :show]
+    resources :reservations, only: [:new, :create, :show, :edit, :update]
   end
   resources :reservations, only: [:destroy]
 
